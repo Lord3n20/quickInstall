aa25b93fec1c10cb60ef8611cbd5e71d82631851
Lord3n20 <enzo@brandsch.com>
2025-03-11 12:29:35 +0100

    Included dependencies

--- main.sh
+++ main.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
 # v0.1
 # This script will install all dependencies for the other scripts to run.
-# At the moment this just includes: yay, git, wget
+# At the moment this just includes: yay, git
+
+yes | sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+yes | makepkg -si
+cd .. && yes | rm -r yay
